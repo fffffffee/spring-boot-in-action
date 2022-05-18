41b70bcb9c1b967cc4d408b6f902db9c7f8f1b2a
fffffffee <light_black@126.com>
2022-05-18 16:34:53 +0800

    ch3 01

---
+++ src/main/java/com/auspiciouslife/readinglist/ReaderRepository.java
@@ -0,0 +1,6 @@
+package com.auspiciouslife.readinglist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReaderRepository extends JpaRepository<Reader, String> {
+}
